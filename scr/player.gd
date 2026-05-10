0f472a512559359749fdfdc2586b47d7ec3e8a13
G4RFLD <djcarbonneau11@gmail.com>
2026-05-10 15:36:06 -0400

    MickeyDees
    Added a mcdonalds level and a spatula you can pick up, kind of

--- scr/player.gd
+++ scr/player.gd
@@ -3,14 +3,18 @@ extends CharacterBody3D
 var speed = 12
 var JUMP_VELOCITY = 8
 var gravity = 19
-
+var pickedObject
 const SENSITIVITY =0.004
+signal interact_object
+@onready var ray_cast_3d: RayCast3D = $Head/Camera3D/RayCast3D
+
 
 @onready var head: Node3D = $Head
 @onready var camera_3d: Camera3D = $Head/Camera3D
 
 
 func _ready():
+	add_to_group("player")
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 	
@@ -23,9 +27,16 @@ func _unhandled_input(event):
 		camera_3d.rotate_x(-event.relative.y * SENSITIVITY)
 		camera_3d.rotation.x = clamp(camera_3d.rotation.x, deg_to_rad(-40), deg_to_rad(60))
 
+func _input(event):
+	if event.is_action_pressed("interaction") and pickedObject:
+		pickedObject.reparent(get_tree().current_scene)
+		pickedObject = null
+
 
 func _physics_process(delta: float) -> void:
-
+	if ray_cast_3d.is_colliding():
+		var collider = ray_cast_3d.get_collider()
+		interact_object.emit(collider)
 
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -50,3 +61,10 @@ func _physics_process(delta: float) -> void:
 		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
 
 	move_and_slide()	
+	
+func pick_up_object(object):
+	object.reparent(self)
+	object.global_position = $Head/CarryObjectMarker.global_position
+	
+	await get_tree().create_timer(0.1).timeout
+	pickedObject = object
